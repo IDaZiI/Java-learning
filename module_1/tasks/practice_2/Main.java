bf0d481a2bcdf1e566c258563dc467ca9f8344bc
DaZi <danilovazna777@mail.ru>
2026-08-18 16:02:13 +0700

    Learning Java

--- module_1/tasks/practice_2/Main.java
+++ module_1/tasks/practice_2/Main.java
@@ -3,6 +3,7 @@ package module_1.tasks.practice_2;
 public class Main {
     public static void main(String[] args) {
         Dog dog = new Dog("Боотур", 3);
+        dog.name = "ВВВ";
         //Dog dog1 = new Dog();     //это не сработает, так как больше нет конструктора по умолчанию, и компиляция не пройдет
     }
 }
